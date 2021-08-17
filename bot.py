6f1e4bf53b0652be72ec6b33c1245505622f0048
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-17 20:57:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,10 +17,10 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("__**Im MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more infomation__\n\n Follow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github",
+  await event.reply("__**🤔Im MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more infomation__\n\n Follow [@omindas](https://github.com/omindadelshan) on Github",
                     buttons=(
-                      [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      [Button.url('👨‍💻 Channel 👨‍💻', 'https://t.me/sdprojectupdates'),
+                      Button.url('🎈DEV🎈', 'https://t.me/omindas')]
                     ),
                     link_preview=False
                    )
@@ -29,8 +29,8 @@ async def help(event):
   helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentioall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      [Button.url('👨‍💻 Channel 👨‍💻', 'https://t.me/sdprojectupdates'),
+                      Button.url('🎈DEV🎈', 'https://t.me/omindas')]
                     ),
                     link_preview=False
                    )
@@ -38,7 +38,7 @@ async def help(event):
 @client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
 async def mentionall(event):
   if event.is_private:
-    return await event.respond("__This command can be use in groups and channels!__")
+    return await event.respond("__😇This command can be use in groups and channels😇!__")
   
   admins = []
   async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
